01392f0588f1f620f77714dd6b2e5314c0e2b37c
Shabhaizafar <zafarshabhai03@gmail.com>
2025-07-26 15:46:56 +0530

    Basic Function

---
+++ 4.Basic/Function.js
@@ -0,0 +1,27 @@
+// Function : that is Block of Code which Perform some task.
+
+// there are 2 Categories :
+// 1. In-Built Function 
+    // ex : prompt,..etc
+
+// 2. User-defined Function
+
+
+// 1. prompt : user input   : return string | null
+
+// var Name = prompt("Enter Your Name :");
+// console.log(Name,typeof(Name));
+
+// var Age = Number(prompt("Enter Your Age :"));
+// console.log(Age,typeof(Age));
+
+
+// 2. confirm :   true | false
+
+// var ans = confirm("Are you Ready ?");
+// console.log(ans);
+
+
+// 3. alert :
+
+alert("Hello User !!");
