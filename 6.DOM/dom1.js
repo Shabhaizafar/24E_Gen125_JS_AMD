aee1b3186dbb2e3c3b94d66323dab4129f73f28c
Shabhaizafar <zafarshabhai03@gmail.com>
2025-09-21 15:37:42 +0530

    DOM

---
+++ 6.DOM/dom1.js
@@ -0,0 +1,43 @@
+// DOM : Document Object Manuplation/Model
+
+// Document :  HTML Code 
+// Object   :  Key+value 
+// Model     : Stracture 
+
+
+
+console.log("DOM MODEL");
+
+// How To access Full HTML File Code using JS :
+// console.log(document);
+
+
+
+// How To Access Head Tag using JS : 
+// console.log(document.head);
+
+
+// How To Access Body Tag Using JS : 
+// console.log(document.body);
+// console.log(document['body']);
+
+
+// ==========================================
+// How to Access Any HTML Element using jS : (head/body)
+
+//1. using id :
+// console.log(document.getElementById('id1'));
+
+
+//2. using class :
+// console.log(document.getElementsByClassName('class1')[2]);
+
+//3. using tagname : 
+// console.log(document.getElementsByTagName('h3')[0]);
+console.log(document.getElementsByTagName('h3')[1]);
+
+
+
+
+
+
